eb5b7683656a44a063746033b75237bda2e33951
ASWINMO <110896795+ASWINMO@users.noreply.github.com>
2022-08-17 15:21:44 +0530

    fourth committ

---
+++ in list sum pro square.py
@@ -0,0 +1,21 @@
+list=[1,2,3,4,5,6,7,8,9,10]
+def add(list):
+    sum=0
+    for i in list:
+        sum=sum+i
+    return sum
+def sum(list):
+    sum=1
+    for i in list:
+        sum=sum*i
+    return sum
+def sqaure(list):
+    sum=[]
+    for i in list:
+
+        sum.append(i*i)
+    return sum
+
+print(sum(list))
+print(sqaure(list))
+print(add(list))
